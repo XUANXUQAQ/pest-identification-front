13bab19ac77de8a079864134144a6197d73abebe
heibaige123 <2126590780@qq.com>
2021-04-22 21:37:39 +0800

    完成主页框架

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        active-text-color="#409eff"
+        background-color="#304156"
+        text-color="#bfcbd9"
+      >
+        <el-menu-item index="2">
+          <i class="el-icon-menu" />
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-document" />
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting" />
+          <span slot="title">导航四</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-setting" />
+          <span slot="title">导航5</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  components: {},
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.el-header {
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color: yellowgreen;
+}
+
+.el-aside {
+  background-color: #304156;
+  height: 100vh;
+}
+</style>
